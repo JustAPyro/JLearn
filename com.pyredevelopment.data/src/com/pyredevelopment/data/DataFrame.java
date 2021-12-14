26204845fcf8aea8aed33df0404001ba4cad61a5
Luke Hanna <luke.m.hanna@gmail.com>
2021-12-14 17:29:06 -0500

    Created a basic file reader set-up

--- com.pyredevelopment.data/src/com/pyredevelopment/data/DataFrame.java
+++ com.pyredevelopment.data/src/com/pyredevelopment/data/DataFrame.java
@@ -1,5 +1,9 @@
 package com.pyredevelopment.data;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * This is the dataframe object that the machine learning algorithms take as an input.
  * Note that this is an array-based object and intended to simulate a Panda dataFrame
@@ -13,8 +17,32 @@ public class DataFrame {
     // - - - - - - - - - - Static Methods - - - - - - - - - - -
 
     // TODO: Implement reading CSV to dataframe
-    public static DataFrame readCSV() {
-        throw new RuntimeException("NOT IMPLEMENTED");
+    public static DataFrame readCSV(String fileLocation) {
+
+        // Try, in case we encounter IO exception
+        try {
+
+            // Create a buffered reader from the provided file
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileLocation));
+
+            // Create a variable to store the current line being processed
+            String currentLine;
+
+            // For each line we iterate through
+            while((currentLine = bufferedReader.readLine()) != null) {
+
+                // Process each line here
+                System.out.println(currentLine);
+
+            }
+
+            return null;
+
+        } catch (IOException e) {
+            // If we do encounter an error, just print the error and return null
+            e.printStackTrace();
+            return null;
+        }
     }
 
     // TODO: Implement reading ARFF to dataframe
---
+++ com.pyredevelopment.examples/src/com/pyredevelopment/regression/LinearRegressionExample.java
@@ -0,0 +1,20 @@
+package com.pyredevelopment.regression;
+
+import com.pyredevelopment.data.DataFrame;
+
+import java.io.File;
+
+/**
+ * This class gives an example of simple Linear Regression using JLearn
+ */
+public class LinearRegressionExample {
+
+    public static void main(String[] args) {
+
+        // First we load the data into a dataframe using CSV
+        DataFrame data = DataFrame.readCSV("com.pyredevelopment.examples/src/resources/CarDetails.csv");
+
+
+    }
+
+}
