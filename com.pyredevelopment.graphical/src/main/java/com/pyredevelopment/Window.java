a6d57ee24c2fde23a7b7bb6b36f1d75f0d6e3be1
Luke Hanna <luke.m.hanna@gmail.com>
2021-12-17 00:37:54 -0500

    Adding some JavaDocs and tidied code

--- com.pyredevelopment.graphical/src/main/java/com/pyredevelopment/Window.java
+++ com.pyredevelopment.graphical/src/main/java/com/pyredevelopment/Window.java
@@ -1,11 +1,12 @@
 package com.pyredevelopment;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
 import static javafx.application.Application.launch;
 
+/**
+ * This class provides an easy wrapper/API class to easily create windows
+ */
 public class Window {
+
 
     public Window() {
         launch(WindowManager.class);
--- com.pyredevelopment.graphical/src/main/java/com/pyredevelopment/WindowManager.java
+++ com.pyredevelopment.graphical/src/main/java/com/pyredevelopment/WindowManager.java
@@ -4,8 +4,10 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class WindowManager extends Application {
+
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         stage.show();
     }
+
 }
