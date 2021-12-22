6e51a08b442ec230d6197eee2f489652e2aea25d
Luke Hanna <luke.m.hanna@gmail.com>
2021-12-22 12:00:45 -0500

    Added basic constructor and instance values for cartesian class

--- com.pyredevelopment.graphical/src/main/java/com/pyredevelopment/graphics/Cartesian.java
+++ com.pyredevelopment.graphical/src/main/java/com/pyredevelopment/graphics/Cartesian.java
@@ -1,7 +1,41 @@
 package com.pyredevelopment.graphics;
 
+/**
+ * - INTERNAL CLASS -
+ * This class is used to display and do math on a cartesian coordinate system.
+ */
 public class Cartesian {
 
+    // - - - - - - - - - - Instance Variables - - - - - - - - - -
+
+    // Location and size of the cartesian
+    int x, y, width, height;
+
+    // Start/end range and scale for each axis
+    int xStart, xEnd, xScale;
+    int yStart, yEnd, yScale;
+
+
+    // - - - - - - - - - - Constructors - - - - - - - - - -
+
+    /**
+     * Create a cartesian starting at X, Y, with Width and Height
+     * @param x The horizontal location of top left corner of the cartesian
+     * @param y The vertical location of the top left corner of cartesian
+     * @param width Pixel width of the cartesian
+     * @param height Pixel height of cartesian
+     */
+    public Cartesian(int x, int y, int width, int height) {
+
+        // Pass the provided parameters into the function
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+    }
+
+    // - - - - - - - - - - Setters - - - - - - - - - -
 
 
 }
