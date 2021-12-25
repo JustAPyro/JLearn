5334dfb5329be23bd84f95fa71e33ea436d303bf
Luke Hanna <luke.m.hanna@gmail.com>
2021-12-24 23:06:27 -0500

    Both Data classes now implement DataObject

--- com.pyredevelopment.data/src/main/java/com/pyredevelopment/data/DataBuffer.java
+++ com.pyredevelopment.data/src/main/java/com/pyredevelopment/data/DataBuffer.java
@@ -6,7 +6,7 @@ import java.io.File;
  * This class creates a buffer that will read through data and perform functions on subsets of it.
  * This is primarily intended to make processing data files too large for memory easier.
  */
-public class DataBuffer {
+public class DataBuffer implements DataObject{
 
     // - - - - - - - - - - Constructors - - - - - - - - - -
 
