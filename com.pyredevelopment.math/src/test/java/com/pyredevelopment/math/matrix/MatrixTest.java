9c39892b9a4b8998288100b4565a650887b09fe5
Luke Hanna <luke.m.hanna@gmail.com>
2021-12-27 11:53:19 -0500

    Added unit tests for various other sizes

--- com.pyredevelopment.math/src/test/java/com/pyredevelopment/math/matrix/MatrixTest.java
+++ com.pyredevelopment.math/src/test/java/com/pyredevelopment/math/matrix/MatrixTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class MatrixTest {
 
     double[][] testMatrix; // matrix used for testing
+    double[][] expectedMatrix; // Matrix to hold the expected outcome
 
     @Test
     @DisplayName("Testing 2x2 Matrix Determinant")
@@ -27,8 +28,6 @@ class MatrixTest {
                 {3, 0, 9}};
         assertEquals(-48, Matrix.determinant(testMatrix));
     }
-
-
 
     @Test
     @DisplayName("Testing 4x4 Matrix Determinant")
@@ -58,5 +57,58 @@ class MatrixTest {
         assertEquals(-1075387108, Matrix.determinant(testMatrix));
     }
 
+    @Test
+    @DisplayName("Testing 3x3 Matrix Of Minors")
+    void test3x3matrixofminors() {
+        testMatrix = new double[][] {
+                {4,2,3},
+                {6,4,0},
+                {9,5,0}};
+        expectedMatrix = new double[][] {
+                {0, 0, -6},
+                {-15, -27, 2},
+                {-12, -18, 4}};
+
+        assertArrayEquals(expectedMatrix, Matrix.matrixOfMinors(testMatrix));
+
+    }
+
+    @Test
+    @DisplayName("Testing 4x4 Matrix Of Minors")
+    void test4x4matrixofminors() {
+        testMatrix = new double[][] {
+                {3,1,3,3},
+                {2,4,1,7},
+                {7,5,0,7},
+                {1,3,3,2}};
+        expectedMatrix = new double[][] {
+                {32, 98, 62,-38},
+                {57,-21,  8, 72},
+                {47,-33,-30, 28},
+                {13,105,-40,-62}};
+
+        assertArrayEquals(expectedMatrix, Matrix.matrixOfMinors(testMatrix));
+
+    }
+
+    @Test
+    @DisplayName("Testing 5x5 Matrix Of Minors")
+    void test5x5matrixofminors() {
+        testMatrix = new double[][] {
+                {3,8,8,4,1},
+                {1,7,0,5,6},
+                {8,2,1,8,3},
+                {5,7,0,3,2},
+                {8,3,7,0,1}};
+        expectedMatrix = new double[][] {
+                {-1200,-744,1338,-1596,-1998},
+                {761,-284,-427,-703,-3951},
+                {542,1150,-52,-2128,-522},
+                {-1423,2548,2357,-817,2529},
+                {1294,686,970,2128,2358}};
+
+        assertArrayEquals(expectedMatrix, Matrix.matrixOfMinors(testMatrix));
+
+    }
 
 }
