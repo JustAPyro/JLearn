5aadd0c7d07e53777b9da173f03a59df40a56154
Luke Hanna <luke.m.hanna@gmail.com>
2021-12-26 20:14:19 -0500

    Added KeyException class that extends RuntimeException

---
+++ com.pyredevelopment.data/src/main/java/com/pyredevelopment/errors/KeyException.java
@@ -0,0 +1,10 @@
+package com.pyredevelopment.errors;
+
+/**
+ * Error class for KeyException
+ * TODO: Add information here, refer to https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.at.html#pandas.DataFrame.at
+ */
+public class KeyException extends RuntimeException{
+
+
+}
