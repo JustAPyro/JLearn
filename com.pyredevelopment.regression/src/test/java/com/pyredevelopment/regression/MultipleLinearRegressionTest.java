bd51d4759b74a7b5cc7757965e74e8803d9d726d
Luke Hanna <luke.m.hanna@gmail.com>
2021-12-28 21:52:56 -0500

    Added partial test methods for MLR

--- com.pyredevelopment.regression/src/test/java/com/pyredevelopment/regression/MultipleLinearRegressionTest.java
+++ com.pyredevelopment.regression/src/test/java/com/pyredevelopment/regression/MultipleLinearRegressionTest.java
@@ -1,7 +1,16 @@
 package com.pyredevelopment.regression;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MultipleLinearRegressionTest {
+
+    @Test
+    @DisplayName("Testing a 2x10 perfect multiple linear regression")
+    void test2x10MLR() {
+
+    }
 
 }
