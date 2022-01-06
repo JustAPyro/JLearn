0927862d48b729a08148d0b8d6d744a8aea3d25b
Luke Hanna <luke.m.hanna@gmail.com>
2022-01-06 12:09:04 -0500

    Added an example analysis of a specific data set

--- com.pyredevelopment.examples/src/main/java/com/pyredevelopment/examples/MedicalInsuranceExample.java
+++ com.pyredevelopment.examples/src/main/java/com/pyredevelopment/examples/MedicalInsuranceExample.java
@@ -1,12 +1,18 @@
 package com.pyredevelopment.examples;
 
 import com.pyredevelopment.data.DataArray;
+import com.pyredevelopment.window.Plot;
 
 public class MedicalInsuranceExample {
 
     public static void main(String[] args) {
 
-        DataArray data = DataArray.readCSV("C:/Users/Luke/Programming/DataSets/Kaggle/MedicalInsuranceCosts.csv");
+        DataArray data = DataArray.readCSV("C:\\Users\\Luke\\Programming\\DataSets\\Kaggle\\MedicalInsuranceCosts.csv");
+
+        Plot plt = new Plot();
+        plt.plot(data, "bmi", "charges", "ro");
+        plt.show();
+
 
 
     }
