5b8fdd618e00a338da7459ec3816ef99a12b1c85
Luke Hanna <luke.m.hanna@gmail.com>
2022-02-27 16:19:27 -0500

    Added unit tests

--- src/test/java/com/pyredevelopment/data/DataArrayTest.java
+++ src/test/java/com/pyredevelopment/data/DataArrayTest.java
@@ -2,6 +2,10 @@ package com.pyredevelopment.data;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DataArrayTest {
@@ -36,6 +40,59 @@ class DataArrayTest {
 
     @Test
     void createWithHashMap() {
+
+        // Test Variables
+        HashMap<String, List<Integer>> hashMap;
+        DataArray dataArray;
+        int[] expectedShape;
+        String[] expectedHeaders;
+
+        // ITEM:----- Returns data object of correct size from 1x5 -----:
+        // Set-up
+        hashMap = new HashMap<>();
+        hashMap.put("One", List.of(1));
+        hashMap.put("Two", List.of(2));
+        hashMap.put("Three", List.of(3));
+        hashMap.put("Four", List.of(4));
+        hashMap.put("Five", List.of(5));
+        expectedShape = new int[]{1, 5};
+
+        // Execution
+        dataArray = DataArray.fromMap(hashMap);
+
+        // Assert
+        assertArrayEquals(expectedShape, dataArray.shape());
+
+        // ITEM:----- Returns data object of correct size from 5x1 -----:
+        // Set-up
+        hashMap = new HashMap<>();
+        hashMap.put("All", List.of(1, 2, 3, 4, 5));
+        expectedShape = new int[]{5, 1};
+
+        // Execution
+        dataArray = DataArray.fromMap(hashMap);
+
+        // Assert
+        assertArrayEquals(expectedShape, dataArray.shape());
+
+        // ITEM:----- Correctly labels keys -----:
+        // Set-up
+        hashMap = new HashMap<>();
+        hashMap.put("One", List.of(1));
+        hashMap.put("Two", List.of(2));
+        hashMap.put("Three", List.of(3));
+        hashMap.put("Four", List.of(4));
+        hashMap.put("Five", List.of(5));
+
+        // Execution
+        dataArray = DataArray.fromMap(hashMap);
+
+        // Assert (Note that order isn't guaranteed, so we just check for contains)
+        assertTrue(dataArray.getColumns().contains("One"));
+        assertTrue(dataArray.getColumns().contains("Two"));
+        assertTrue(dataArray.getColumns().contains("Three"));
+        assertTrue(dataArray.getColumns().contains("Four"));
+        assertTrue(dataArray.getColumns().contains("Five"));
 
     }
 
