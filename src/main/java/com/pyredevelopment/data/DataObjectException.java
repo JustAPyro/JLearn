23d91e39fb0370b72fa0e1e746f230e11d98f34d
Luke Hanna <luke.m.hanna@gmail.com>
2022-03-08 09:15:11 -0500

    Created a custom exception class

---
+++ src/main/java/com/pyredevelopment/data/DataObjectException.java
@@ -0,0 +1,11 @@
+package com.pyredevelopment.data;
+
+/**
+ * TODO: Document better
+ * Custom exception class for data object extension classes.
+ */
+public class DataObjectException extends RuntimeException {
+    public DataObjectException(String message) {
+        super(message);
+    }
+}
