9ecde0fc8e223c1ec94fbd84d4be8af2b627cdfd
Luke Hanna <luke.m.hanna@gmail.com>
2022-03-23 01:00:44 -0400

    added method for parsing a delimited row/string

--- src/main/java/com/pyredevelopment/Dataset.java
+++ src/main/java/com/pyredevelopment/Dataset.java
@@ -33,17 +33,44 @@ public class Dataset {
         String lineToBeParsed;
     }
 
+    // - - - - - - - - - - Private Methods - - - - - - - - - -
 
+    /*
+    This method is here to parse lines (mostly for CSV's) on a delimiter.
+    It returns an arraylist of strings for each word between the delimiters.
+     */
+    private static ArrayList<String> parseDelimitedLine(String csvLine, char delimiter) {
 
+        // Create an array of the delimited strings
+        ArrayList<String> outputInstance = new ArrayList<>();
+
+        // Split the string into characters
+        char[] chars = csvLine.toCharArray();
+
+        // Create a buffer to build specific words
+        StringBuilder wordBuffer = new StringBuilder();
+
+        // For each character in the string
+        for (char c: chars)
+            // Process it based on the char, the deliminator. Either add the word to output or char to word
+            processChar(c, delimiter, wordBuffer, outputInstance);
+
+        // Add the final word left in the buffer
+        outputInstance.add(wordBuffer.toString());
+
+        // Return the list of words
+        return outputInstance;
+
+    }
 
     /*
     This processes an individual char, if it's the deliminator character it will push
     buffer into the provided array, otherwise the char will simply be added to the buffer.
     */
-    private static void processChar(char c, char deliminator, StringBuilder buffer, ArrayList<String> stringList) {
+    private static void processChar(char c, char delimiter, StringBuilder buffer, ArrayList<String> stringList) {
 
         // If the character is the deliminator (usually comma)
-        if (c == deliminator) {
+        if (c == delimiter) {
 
             // Add the word to the headers and clear buffer
             stringList.add(buffer.toString());
