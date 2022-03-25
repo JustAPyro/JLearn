f8ccd1db5ad4aa682277a1dadaacd6975f3a90fd
Luke Hanna <luke.m.hanna@gmail.com>
2022-03-24 22:51:26 -0400

    fixed bug in splitting method

--- src/main/java/com/pyredevelopment/LinearRegression.java
+++ src/main/java/com/pyredevelopment/LinearRegression.java
@@ -5,9 +5,34 @@ public class LinearRegression {
 
     public LinearRegression(Dataset inputs, Dataset output) {
 
-        System.out.println(inputs.getHead());
+        int features = inputs.getHeaders().size();
 
-        System.out.println(output.getHead());
+        Double[][] matrixXtX = new Double[features][features];
+
+
+
+
+    }
+
+    private Double[] multiplyMatrixLinear(Dataset input) {
+
+        int features = input.getHeaders().size();
+
+        Double[][] matrixXtX = new Double[features][features];
+
+        for (int m = 0; m < matrixXtX.length; m++) {
+            for (int n = 0; n < matrixXtX[m].length; n++) {
+
+                for (int i = 0; i < input.getEntries(); i++) {
+
+                }
+
+
+            }
+        }
+
+
+
 
     }
 
