c479d217afe84acab918ee498f69de29694d2029
Luke Hanna <luke.m.hanna@gmail.com>
2022-04-05 23:26:48 -0400

    Defined unit tests for matrix determinant

--- src/test/java/com/pyredevelopment/mathresources/MatrixTest.java
+++ src/test/java/com/pyredevelopment/mathresources/MatrixTest.java
@@ -17,7 +17,9 @@ class MatrixTest {
         double expected;
         double result;
 
-        //
+
+        /* - - - - - Testing Throws Error for non-square determinant - - - - - */
+        // TODO: Testing Throws Error for non-square determinant
 
         /* - - - - - Testing 1x1 Matrix Determinant - - - - - */
 
@@ -46,6 +48,64 @@ class MatrixTest {
         // Assert
         assertEquals(expected, result);
 
+        /* - - - - - Testing 3x3 Matrix Determinant - - - - - */
+
+        // Setup
+        expected = -48.0;
+        testMatrix = new Dataset(new double[][] {
+                {6, 3, 4},
+                {6, 1, 8},
+                {3, 0, 9}
+        });
+
+        // Execute
+        result = Matrix.determinant(testMatrix);
+
+        // Assert
+        assertEquals(expected, result);
+
+        /* - - - - - Testing 4x4 Matrix Determinant - - - - - */
+
+        // Setup
+        expected = -288.0;
+        testMatrix = new Dataset(new double[][] {
+                {3, 8, 8, 4},
+                {3, 7, 6, 1},
+                {5, 7, 6, 9},
+                {7, 9, 2, 9}
+        });
+
+        // Execute
+        result = Matrix.determinant(testMatrix);
+
+        // Assert
+        assertEquals(expected, result);
+
+        /* - - - - - Testing 4x4 Matrix Determinant with floats - - - - - */
+        // TODO: Testing 4x4 Matrix Determinant with floats
+
+        /* - - - - - Testing a 10x10 Matrix Determinant - - - - - */
+
+        // Setup
+        expected = -1075387108;
+        testMatrix = new Dataset(new double[][] {
+                {8,8,8,9,3,7,3,8,8,4},
+                {7,2,5,7,4,3,6,9,3,9},
+                {9,0,0,2,3,4,0,1,8,8},
+                {4,4,6,8,3,1,8,0,0,1},
+                {7,8,0,5,2,7,1,9,4,0},
+                {4,1,5,7,5,9,9,0,3,2},
+                {4,4,2,7,8,1,5,6,1,4},
+                {4,5,5,0,8,9,5,3,0,9},
+                {5,7,1,7,5,4,2,0,0,1},
+                {1,7,2,1,5,2,1,4,7,8}
+        });
+
+        // Execute
+        expected = Matrix.determinant(testMatrix);
+
+        // Assert
+        assertEquals(expected, result);
 
 
     }
