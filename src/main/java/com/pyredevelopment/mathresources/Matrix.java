875c569033c8f3ddda0dcd7c604e4152cc5099ac
Luke Hanna <Luke.m.hanna@gmail.com>
2022-04-10 22:32:23 -0400

    Cleaned the matrix utility class

--- src/main/java/com/pyredevelopment/mathresources/Matrix.java
+++ src/main/java/com/pyredevelopment/mathresources/Matrix.java
@@ -1,45 +1,31 @@
 package com.pyredevelopment.mathresources;
 
-import java.util.Arrays;
-
 public class Matrix {
 
-    private static int rounding = 8;
-    private int nRows;  // Number of rows
-    private int nCols;  // Number of columns
-    private Number[][] data;    // Main dataset
+    /* - - - - - - - - - - Static/Final Variables - - - - - - - - - - */
 
-    public Matrix(int nRows, int nCols) {
-        this.nRows = nRows;
-        this.nCols = nCols;
-        data = new Number[nCols][nRows];
-    }
+    // The value we will round by to avoid floating point problems
+    private static final int rounding = 8;
+
+    /* - - - - - - - - - - Public static methods - - - - - - - - - - */
 
     public static double[][] inverse(double[][] matrix) {
 
         guardMatrixSquare(matrix); // Requires that the matrix must be square
         guardMatrixInverse(matrix); // Requires that the matrix HAS an inverse (det != 0)
 
+        // Create an output array
         double[][] output;
-
-        for (double[] row : matrix)
-            System.out.println(Arrays.toString(row));
 
         // First get the cofactor
         output = cofactor(matrix);
 
-
-
         // Then transpose it
         output = transpose(output);
-
-
 
         // Then multiply by the constant 1/determinant(matrix)
         double constant = 1.0/determinant(matrix);
         output = multiply(output, constant);
-
-
 
         // Return the result
         return output;
@@ -64,7 +50,6 @@ public class Matrix {
         // Return the results
         return output;
 
-
     }
 
     public static double[][] transpose(double[][] matrix) {
@@ -87,7 +72,6 @@ public class Matrix {
 
     }
 
-    // Thanks to Daniel for this solution (mostly)
     public static double determinant(double[][] matrix) {
 
         guardMatrixSquare(matrix); // Guard against non-square matrices
@@ -179,6 +163,35 @@ public class Matrix {
 
     }
 
+    /* - - - - - - - - - - Private static methods - - - - - - - - - - */
+
+    private static double[][] subMatrix(double[][] matrix, int excluding_row, int excluding_col) {
+
+        guardMatrixRectangular(matrix); // Make sure this dataset is a real matrix (aka, all rows are same length)
+
+        // Create an output matrix of one less length and width
+        double[][] output = new double[matrix.length-1][matrix[0].length-1];
+
+        // Helps map the rows after we skip
+        int inc = 0;
+
+        // Iterate through each row
+        for (int row = 0; row < matrix.length; row++) {
+
+            // If this is the row we're excluding, set the increment and then skip the rest to continue loop
+            if (row == excluding_row) { inc = -1; continue; }
+
+            // Copy the parts of the array up until the excluding_col and then everything after, ignoring the excluding_col
+            System.arraycopy(matrix[row], 0, output[row+inc], 0, excluding_col);
+            System.arraycopy(matrix[row], excluding_col+1, output[row+inc], excluding_col, matrix[row].length-excluding_col-1);
+
+        }
+
+        return output;
+    }
+
+    /* - - - - - - - - - - Guards (Add to beginning to check for valid input) - - - - - - - - - - */
+
     private static void guardMatrixRectangular(double[][] matrix) {
 
         // Save the length of the first row
@@ -208,174 +221,6 @@ public class Matrix {
             // Then there is no inverse
             throw new IllegalArgumentException("This matrix has no inverse: Determinant is 0");
     }
-
-
-    private static double[][] subMatrix(double[][] matrix, int excluding_row, int excluding_col) {
-
-        guardMatrixRectangular(matrix); // Make sure this dataset is a real matrix (aka, all rows are same length)
-
-        // Create an output matrix of one less length and width
-        double[][] output = new double[matrix.length-1][matrix[0].length-1];
-
-        // Helps map the rows after we skip
-        int inc = 0;
-
-        // Iterate through each row
-        for (int row = 0; row < matrix.length; row++) {
-
-            // If this is the row we're excluding, set the increment and then skip the rest to continue loop
-            if (row == excluding_row) { inc = -1; continue; }
-
-            // Copy the parts of the array up until the excluding_col and then everything after, ignoring the excluding_col
-            System.arraycopy(matrix[row], 0, output[row+inc], 0, excluding_col);
-            System.arraycopy(matrix[row], excluding_col+1, output[row+inc], excluding_col, matrix[row].length-excluding_col-1);
-
-        }
-
-        return output;
-    }
-
-    public void add(int r, int c, Number value) {
-        data[r][c] = value;
-    }
-
-    public Double get(int r, int c) {
-        return data[r][c].doubleValue();
-    }
-
-    public boolean isSquare() {
-        return nRows == nCols;
-    }
-
-    public int getSize() {
-        if (!isSquare())
-            throw new RuntimeException("Non-square matrix doesn't have integer size.");
-
-        return nRows;
-    }
-
-
-
-    /*
-    public static Matrix transpose(Matrix matrix) {
-    Matrix transposedMatrix = new Matrix(matrix.getNcols(), matrix.getNrows());
-    for (int i=0;i<matrix.getNrows();i++) {
-        for (int j=0;j<matrix.getNcols();j++) {
-            transposedMatrix.setValueAt(j, i, matrix.getValueAt(i, j));
-        }
-    }
-    return transposedMatrix;
-}
-     */
-    public Matrix transpose() {
-        Matrix outMatrix = new Matrix(nRows, nCols);
-        for (int r = 0; r < nRows; r++) {
-            for (int c = 0; c < nCols; c++) {
-                outMatrix.add(r, c, get(c, r));
-            }
-        }
-        return outMatrix;
-    }
-
-    public Number determinant() {
-        System.out.println("Starting with " + nCols);
-        return calculateDeterminant(this);
-    }
-
-    public Number calculateDeterminant(Matrix input) {
-
-        // If it's not square
-        if (!input.isSquare())
-            throw new RuntimeException("Non-square matrix doesn't have determinant.");
-
-        // If the matrix is only one cell, then that is the determinant
-        if (input.getSize() == 1)
-            return data[0][0];
-
-        // If the matrix is two cells, then matrix is (ac-bd)
-        if (input.getSize() == 2)
-            return (multiply(input.get(0, 0), input.get(1, 1))) - (multiply(input.get(1, 0), input.get(0, 1)));
-
-        // If the matrix is larger, recursively calculate down
-        double sum = 0.0;
-        for (int i = 0; i < input.getSize(); i++) {
-            int multiple = ((i % 2) == 0) ? 1 : -1; // If it's even subtract, if odd add
-            sum += multiply(multiple, input.get(0, i), calculateDeterminant(input.newSubMatrix(0, i)));
-        }
-        return sum;
-
-    }
-
-    /*
-    Matrix mat = new Matrix(matrix.getNrows(), matrix.getNcols());
-    for (int i=0;i<matrix.getNrows();i++) {
-        for (int j=0; j<matrix.getNcols();j++) {
-            mat.setValueAt(i, j, changeSign(i) * changeSign(j) *
-                             determinant(createSubMatrix(matrix, i, j)));
-        }
-    }
-
-    return mat;
-     */
-    public Matrix cofactor() {
-        Matrix outMatrix = new Matrix(nRows, nCols);
-        for (int r = 0; r < nRows; r++) {
-            for (int c = 0; c < nCols; c++) {
-                int rowMultiple = ((r % 2) == 0) ? 1 : -1;
-                int colMultiple = ((c % 2) == 0) ? 1 : -1;
-                outMatrix.add(r, c, multiply(rowMultiple, colMultiple, newSubMatrix(r, c).determinant()));
-            }
-        }
-        return outMatrix;
-    }
-
-    public void multiply(Number constant) {
-        for (int r = 0; r < nRows; r++) {
-            for (int c = 0; c < nCols; c++) {
-                add(r, c, multiply(get(r, c), constant));
-            }
-        }
-    }
-
-    public Number divide(Number a, Number b) {
-        return a.doubleValue() / b.doubleValue();
-    }
-
-    public Matrix newSubMatrix(int removeRow, int removeCol) {
-
-        Matrix outMatrix = new Matrix(nRows-1, nCols-1);
-
-        int rAdd = -1;
-        for (int r = 0; r < nRows; r++) {
-
-            if (r == removeRow)
-                continue;
-
-            rAdd++;
-            int cAdd = -1;
-            for (int c = 0; c < nCols; c++) {
-
-                if (c == removeCol)
-                    continue;
-
-                cAdd++;
-                outMatrix.add(rAdd, cAdd, get(r, c));
-            }
-        }
-
-        return outMatrix;
-
-    }
-
-    /* Multiply two Numbers by getting the Double value and returning the result as a Number */
-    private static Double multiply(Number ... nums) {
-        double product = 1.0;
-        for (Number num : nums)
-            product *= num.doubleValue();
-
-        return product;
-    }
-
 
 
 
